c810063da2d650c825af6cf55aad310baf67a5e0
Daniil Lifanov <lifanovdaniil@gmail.com>
2024-07-10 04:08:21 +0400

    first correct commit, 1)connection OK

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,11 +1,13 @@
 package jm.task.core.jdbc;
 
+
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
-
-
-
-
+       Connection connection = Util.getConnection();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,31 @@
 package jm.task.core.jdbc.util;
 
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String DB_Driver = "com.mysql.cj.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/user";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "12345";
+    private static Connection connection = null;
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+                Class.forName(DB_Driver).getDeclaredConstructor().newInstance();
+                connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+                System.out.println("Connection OK");
+            } catch (ClassNotFoundException | InvocationTargetException | InstantiationException |
+                     IllegalAccessException |
+                     NoSuchMethodException | SQLException e) {
+                e.printStackTrace();
+                System.out.println("Connection FAILED");
+            }
+        }
+        return connection;
+    }
 }
