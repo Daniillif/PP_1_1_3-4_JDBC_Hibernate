84657c2ceaac3203fedd6504ddb5c9463de81e79
lifanovdaniil <lifanovdaniil@gmail.com>
2024-07-16 16:24:55 +0400

    hibernate branch in progress

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -13,7 +13,21 @@ public class Main {
         // Получение всех User из базы и вывод в консоль (должен быть переопределен toString в классе User)
         // Очистка таблицы User(ов)
         // Удаление таблицы
+        UserService userService = new UserServiceImpl();
 
+        userService.createUsersTable();
+
+        userService.saveUser("Name1", "LastName1", (byte) 20);
+        userService.saveUser("Name2", "LastName2", (byte) 25);
+        userService.saveUser("Name3", "LastName3", (byte) 31);
+        userService.saveUser("Name4", "LastName4", (byte) 38);
+
+        userService.removeUserById(1);
+        userService.getAllUsers().forEach(System.out::println);
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+
+        Util.closeConnection();
 
     }
 
